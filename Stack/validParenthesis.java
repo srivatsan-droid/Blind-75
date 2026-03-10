fbf8d92956009accf0be4955210bc54b0f5d90ce
Srivatsan <sriramsrivatsan2001@gmail.com>
2026-03-10 12:07:46 +0530

    Stack Problem 3 | Day 2

---
+++ Stack/validParenthesis.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class validParenthesis {
+	public static boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for(char c : s.toCharArray()) {
+			if(c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			}
+			else {
+				if(stack.isEmpty()) 
+					return false;
+				char top = stack.pop();
+				if(c == ')' && top!= '(') 
+					return false;
+				if(c == '}' && top != '{')
+					return false;
+				if(c == '[' && top != ']')
+					return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+	public static void main(String[] args) {
+		String s = "()";
+		boolean ans = isValid(s);
+		System.out.println(ans);
+	}
+}
